1900a7e6fa6a065ba9b371b3c105285d4c86c7de
Reza Adil Hakim <43247492+rezadkim@users.noreply.github.com>
2020-12-30 14:03:04 +0700

    Update koin.py

--- koin.py
+++ koin.py
@@ -5,6 +5,7 @@ g = ("\33[0;32m")#Green")
 b = ("\33[0;36m")#Blue")
 red = ("\33[31;1m")#Red")
 ttp = ("\033[0m")#LightGrey")
+underline = ('\033[4;37;48m')
 
 def main():
      os.system("clear")
@@ -29,8 +30,8 @@ def main():
           r = requests.post("http://www.litatom.com/api/sns/v1/lit/account/deposit_by_activity?sid="+ses+"&loc=ID&uuid="+uuid+"&version=3.8.1&lang=in&platform=android", headers=headers, data=json.dumps(data)).text
           cek = json.loads(r)
           if cek['success'] ==True:
-               print(ttp+"["+g+str(len(i))+ttp+"] Berhasil")
+               print(ttp+"["+g+str(i)+ttp+"] Berhasil")
           else:
-               print(ttp+"["+red+str(len(i))+ttp+"] Coba Lagi Besok")
+               print(ttp+"["+red+str(i)+ttp+"] Coba Lagi Besok")
 
 main()
